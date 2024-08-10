7eb035d32a70857a5b6542f443c655d553bf1709
bhumikabhat <bhumikabhat06@gmail.com>
2024-08-10 17:30:17 +0530

    types of loops

---
+++ while loops.py
@@ -0,0 +1,31 @@
+# while loops = execute some code WHILE some condition remains true
+
+#name = input("Enter your name: ")
+#age = int(input("Enter your age: "))
+
+#while name == "":
+#    print("You did not enter your name")
+#    name = input("Enter your name: ")
+#print(f"Hello {name}")
+
+#while age < 0:
+#    print("Age can't be negative")
+#    age = int(input("Enter your age: "))
+#print(f"You are {age} years old")
+
+#food = input("Enter a food you like (q to quit): ")
+
+#while not food == "q":
+#    print(f"You like {food}")
+#    food = input("Enter another food you like (q to quit): ")
+#print("bye")
+
+num = int(input("Enter any number between 1 - 10: "))
+
+while num < 1 or num > 10:
+    print(f"{num} is not valid")
+    num = int(input("Enter any number between 1 - 10: "))
+print(f"Your number is {num}")
+
+
+    
