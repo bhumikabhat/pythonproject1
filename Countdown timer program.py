3e32eb96ef492978375c035f50204651b3f495e8
bhumikabhat <bhumikabhat06@gmail.com>
2024-08-20 19:51:02 +0530

    countdown timer

---
+++ Countdown timer program.py
@@ -0,0 +1,30 @@
+#import time
+
+#time.sleep(3)
+
+#print("TIME'S UP!")
+
+
+#import time
+
+#my_time =int(input("Enter the time in seconds: "))
+
+#for x in reversed(range(0, my_time)):
+#    print(x)
+#    time.sleep(1)
+
+#print("TIME'S UP!")
+
+import time
+
+my_time = int(input("Enter the time in secomds: "))
+
+for x in reversed(range(0, my_time)):
+    seconds = x % 60
+    minutes = int(x / 60) % 60
+    hours = int(x / 3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
+
+print("TIME'S UP!")
+
