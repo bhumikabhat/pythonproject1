fe4dc0bab4e5da4137c594bc6abd5d4272d13e9d
bhumikabhat <bhumikabhat06@gmail.com>
2024-11-23 18:53:57 +0530

    map()

---
+++ map().py
@@ -0,0 +1,16 @@
+# map() = Applies a given function to all items in a collection
+
+#def c_to_f(temp):
+#    return (temp * 9/5) + 32
+
+celsius_temps = [0.0, 10.0, 20.0, 30.0, 40.0, 50.0]
+
+#fahrenheit_temps = list(map(c_to_f, celsius_temps))
+
+#print(fahrenheit_temps)
+
+#for temp in fahrenheit_temps:
+#    print(temp)
+
+fahrenheit_temps = list(map(lambda temp: (temp * 9/5) + 32, celsius_temps))
+print(fahrenheit_temps)
